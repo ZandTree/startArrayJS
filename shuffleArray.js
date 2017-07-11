a88650831339909071ce869fc80e5b38429ff4c0
Zandkosteel <diafox2015@gmail.com>
2017-07-11 23:07:24 +0200

    Create shuffleArray.js

---
+++ shuffleArray.js
@@ -0,0 +1,24 @@
+///# arrays in JS
+// shuffle elements in thegiven  array
+var a = [3,12,6,77,8];
+// additional function 
+function random_foo(x,y){
+    return Math.random() - 0.5;
+    }
+function shuffle(arr){
+    arr.sort(random_foo);
+    }
+    
+// sorting an array without changing the original one
+function foo(x,y){
+    return x - y;
+    }
+    
+arr = [2,3,1,4,,56];
+function sortNoChangeOriginal(arr){
+    var helpArr = arr.slice();
+    helpArr.sort(foo);
+    return helpArr;
+    }
+var arr_new =  sortNoChangeoriginal(arr);   
+    
